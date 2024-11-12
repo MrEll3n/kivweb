bcfddb34145088ae0ce29d35b8607fed91122b96
BuildTools <unconfigured@null.spigotmc.org>
2024-11-12 02:40:40 +0100

    created token_manager.php

--- backend/api/token_manager.php
+++ backend/api/token_manager.php
@@ -1,14 +1,21 @@
 <?php
+// Custom Session token manager
 class tokenMan {
     private $token = null;
+    private $pdo = null;
 
+    // Constructor
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    // Returns the token
     public function getToken() {
-        
-        
         return $this->token;
     }
 
-    public function genToken($token) {
-        $this->token = $token;
+    // Generates token
+    public function genToken() {
+        $this->token = hash('sha256',random_bytes(32));
     }
 }
